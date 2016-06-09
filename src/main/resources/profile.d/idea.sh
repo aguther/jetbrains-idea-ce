78ab259ec085d76e946bf66bdfe990252443157a
Andreas Guther <andreas.guther@airbus.com>
2016-06-09 08:15:56 +0200

    Fixed problems with profile.d script on RHEL6

--- src/main/resources/profile.d/idea.sh
+++ src/main/resources/profile.d/idea.sh
@@ -1,21 +1,20 @@
 #!/usr/bin/env bash
 
-# get version of IBUS
-IBUS_VERSION=$(ibus version)
+# determine if ibus is available
+if hash ibus 2>/dev/null; then
+	# get version of IBUS
+	IBUS_VERSION=$(ibus version)
 
-# parse version
-IBUS_VERSION_PARSED=( ${IBUS_VERSION//./ } )
+	# parse version
+	IBUS_VERSION_PARSED=( ${IBUS_VERSION//./ } )
 
-# check if version is >= 1.5.11
-if [ ${IBUS_VERSION_PARSED[1]} -gt 1 ]; then
-	exit 0
+	# check if version is >= 1.5.11
+	if [ ${IBUS_VERSION_PARSED[1]} -le 1 ]; then
+		if [ ${IBUS_VERSION_PARSED[2]} -le 5 ]; then
+			if [ ${IBUS_VERSION_PARSED[3]} -lt 11 ]; then
+				# enable ibus sync mode to fix IntelliJ-Bug
+				export IBUS_ENABLE_SYNC_MODE=1
+			fi
+		fi
+	fi
 fi
-if [ ${IBUS_VERSION_PARSED[2]} -gt 5 ]; then
-	exit 0
-fi
-if [ ${IBUS_VERSION_PARSED[3]} -ge 11 ]; then
-	exit 0
-fi
-
-# enable ibus sync mode to fix IntelliJ-Bug
-export IBUS_ENABLE_SYNC_MODE=1
